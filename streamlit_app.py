6d8b6fa46956b36016a909d49929ebc221abce47
mohammedvikar2016 <mohammed.vikar2016@gmail.com>
2024-10-24 16:06:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-#import streamlit as st
-
-#st.title("🎈 My new app")
-#st.write(
-#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). Did this work?"
-#)
-
 import streamlit as st
 from langchain_openai.chat_models import ChatOpenAI
 
